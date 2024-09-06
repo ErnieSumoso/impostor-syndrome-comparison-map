47cecae546b592b9ce4bc6863d4006a77312d4f1
Ernie Sumoso <144075326+ErnieSumoso@users.noreply.github.com>
2024-09-05 21:43:56 -0500

    upd: updating constant values

--- constants.py
+++ constants.py
@@ -3,15 +3,15 @@ SUBTITLE_MAIN = "Calculating your Impostor Syndrome metric:"
 SUBTITLE_SIDE = "Required details about you:"
 QUESTIONS_FILE = "questions.txt"
 QUESTION_AGE = "How old are you?"
-QUESTION_EXP = "How many years of experience / dedication?"
+QUESTION_EXP = "How many years of experience?"
 QUESTION_IS = "Based on the question on the right, your IS metric is:"
 AGE_MIN_MAX_DEFAULT = 15, 80, 25
 EXP_MIN_MAX_DEFAULT = 0, 30, 1
 IS_MIN_MAX = 0.0, 8.0
 OPTIONS_WEIGHT = [("Disagree", 0.0), ("Mostly Disagree", 0.25), ("Neutral", 0.5), ("Mostly Agree", 0.75), ("Agree", 1.0)]
-PLOT_TITLE_3D = "Impostor Syndrome Comparison Map - 3D"
-PLOT_TITLE_2D_AGE = "Comparison Map - Age Only"
-PLOT_TITLE_2D_EXP = "Comparison Map - Experience Only"
+PLOT_TITLE_3D = "Impostor Syndrome Comparison Map"
+PLOT_TITLE_2D_AGE = "Comparison Map - Age"
+PLOT_TITLE_2D_EXP = "Comparison Map - Experience"
 CSV_FILE = "data/dataset.csv"
 KMEANS_LABEL = "KMeans"
 MEANSHIFT_LABEL = "MeanShift"
@@ -31,6 +31,6 @@ UPLOAD_SUCCESS = "Your results were uploaded successfully!"
 UPLOAD_FAILED = "Sorry, there was a problem uploading your results, try later!"
 JS_SCROLLDOWN = '''
     <script>
-        window.parent.document.querySelector(".main").scrollTop = 1600;
+        window.parent.document.querySelector(".main").scrollTop = 1550;
     </script>
     '''
